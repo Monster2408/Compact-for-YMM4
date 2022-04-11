7cbe4efd187e424d3aef34d5bbe27029b4c38f82
Monster2408 <47695021+Monster2408@users.noreply.github.com>
2022-04-12 08:30:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 # !C:\Users\enjoy\AppData\Local\Programs\Python\Python38\python.exe
 # -*- coding: utf-8 -*-
 import PySimpleGUI as sg
-import Var
 
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget
+
+import Var
 import function as func
 
-func.makeResouceFiles()
+def main():
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon(resource_path('bar.ico')))
 
-layout = [
-    [sg.Text('プロジェクトファイル', size=(20, 1))],
-    [sg.Input(key='project-file')],
-    [sg.FileBrowse('選択', key='project-file', file_types=(("プロジェクトファイル", ".ymmp"),))]
-]
+    func.makeResouceFiles()
 
-window = sg.Window(Var.TITLE, layout)
+    layout = [
+        [sg.Text('プロジェクトファイル', size=(20, 1))],
+        [sg.Input(key='project-file')],
+        [sg.FileBrowse('選択', key='project-file', file_types=(("プロジェクトファイル", ".ymmp"),))]
+    ]
 
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == '終了':
-        break
+    window = sg.Window(Var.TITLE, layout)
 
-window.close()
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == '終了':
+            break
 
+    window.close()
+
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
 
 
 
